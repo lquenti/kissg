4757b1ab37f55e44d23d66f42cce15472a7bf352
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-02-24 23:20:50 +0100

    header and footer

--- kissg.py
+++ kissg.py
@@ -8,6 +8,8 @@ HTML_EXTENSION = '.html'
 CWD = os.getcwd()
 OUTPUT_FOLDER = 'out'
 
+HEADER_PATH = os.path.join(CWD, 'templates', 'header.html')
+FOOTER_PATH = os.path.join(CWD, 'templates', 'footer.html')
 
 def get_all_filepaths_recursively(path):
     files = []
@@ -43,10 +45,17 @@ if __name__ == '__main__':
     file_dict = create_file_dict(markdown_files)
     if not os.path.exists(OUTPUT_FOLDER):
         os.makedirs(OUTPUT_FOLDER)
+
+    # load the header and footer
+    with open(HEADER_PATH, 'r') as header_file:
+        header = header_file.read()
+    with open(FOOTER_PATH, 'r') as footer_file:
+        footer = footer_file.read()
     
     # for each file in file_dict, create a new file in the output folder
     for f in file_dict:
         slug = get_slug(f)
         with open(os.path.join(OUTPUT_FOLDER, f"{slug}{HTML_EXTENSION}"), 'w') as file:
-            html = convert_markdown_to_html(file_dict[f])
+            html_body = convert_markdown_to_html(file_dict[f])
+            html = header + html_body + footer
             file.write(html)
